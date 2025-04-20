88a1fc73f27e2e3158ef9255bd30adc5aa5585e7
Ekaitz Martin <ekamartinc2003@example.com>
2025-04-20 16:43:24 +0200

    App pages created and instances added

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    #path pages
+    path("pages/", include("pages.urls")),
     #Path core
     path("", include("core.urls")),
     #Path admin
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
