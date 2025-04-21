9a97b99418b3fd169c0bd02bcafee4287a65edd7
Ekaitz Martin <ekamartinc2003@example.com>
2025-04-21 17:33:19 +0200

    UpdateView and DeleteView created

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PageListView, PageDetailView, PageCreate
+from .views import PageListView, PageDetailView, PageCreate, PageUpdate, PageDeleteView
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='page'),#IMPORTANTE, el atributo que ahora se llama pk, antes se llamaba --page_id--, pero lo tengo que cambiar ya que al usar los Class Base View, solo acepta que el parametro se llame --pk-- (viene de primary key) 
-    path("create/", PageCreate.as_view(), name="create")
+    path("create/", PageCreate.as_view(), name="create"),
+    path("update/<int:pk>/", PageUpdate.as_view(), name="update"),
+    path("delete/<int:pk>/", PageDeleteView.as_view(), name="delete"),
 ], "pages")
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Page
 from django.urls import reverse_lazy
 
@@ -17,4 +17,20 @@ class PageCreate(CreateView):
     template_name = "pages/page_create.html"
     model = Page
     fields = ["title", "content", "order"]
+    success_url = reverse_lazy("pages:pages")
+
+class PageUpdate(UpdateView):
+    template_name_suffix  = "_update_form"
+    model = Page
+    fields = ["title", "content", "order"]
+    #En caso de quere mandarlo directamente a la vista donde aparecen todas las páginas=>success_url = reverse_lazy("pages:pages")
+    #Pero voy a hacerlo un poco más currado, voy a enseñar como queda el propio formulario editado, y luego dar posibilidad de ver la página ya editada en page.html
+
+    def get_success_url(self):
+        return reverse_lazy("pages:update", args=[self.object.id]) + "?ok"
+    
+
+
+class PageDeleteView(DeleteView):
+    model = Page
     success_url = reverse_lazy("pages:pages")
