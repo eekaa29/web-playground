1e7dac562eeea61f4ced1598ebaa07f99851241f
Ekaitz Martin <ekamartinc2003@example.com>
2025-04-23 21:28:43 +0200

    Email unique validation

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RequiredEmailForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
@@ -7,7 +7,7 @@ from django import forms
 # Create your views here.
 
 class SignUpView(CreateView):
-    form_class=UserCreationForm
+    form_class=RequiredEmailForm
     template_name = "registration/signup.html"
     def get_success_url(self):
         return reverse_lazy("login") + "?registration"
@@ -16,6 +16,7 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
         #Moificar el formulario:
         form.fields["username"].widget = forms.TextInput(attrs={"class":"form-control mb-2", "placeholder":"Nombre de Usuario"})
+        form.fields["email"].widget = forms.EmailInput(attrs={"class":"form-control mb-2", "placeholder":"Email"})
         form.fields["password1"].widget = forms.PasswordInput(attrs={"class":"form-control mb-2", "placeholder":"Contraseña"})
         form.fields["password2"].widget = forms.PasswordInput(attrs={"class":"form-control mb-2", "placeholder":"Repite la contraseña"})
 
