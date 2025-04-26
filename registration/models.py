513beeb7b9ab72d6d0656c1b86584bc25a2de9f0
Ekaitz Martin <ekamartinc2003@example.com>
2025-04-26 16:24:00 +0200

    Introduction to signal and to unittest + first test created

--- registration/models.py
+++ registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Profile(models.Model):
@@ -8,3 +10,11 @@ class Profile(models.Model):
     bio = models.TextField(null =True, blank = True)
     link = models.URLField(null=True, blank=True, max_length=200)
 
+#Los perfiles de usuario se crean al iniciar sesion, pero ¿Qué pasa si un usuario se registra por primera vez, pero no inicia sesión?
+#Para prevenir que a un usuario recién registrado pero que no haya iniciado sesión no se le cree el perfil, creare una señal
+#De esta manera se crea una señal, que se ejecutará cada vez que se guarde la instancia del usuario.
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    if kwargs.get("created", False):#De esta manera comprobamos que es la primera vez que se guarda la instancia de ese usuario, ya que existirá el campo "created", de lo contrario será False. Así me aseguro de que la señal solo se ejecute la primera vez que se guarde un user, y no cada vez que se guarde después de actualizar un usuario existente.
+        Profile.objects.get_or_create(user=instance)
+        print("Perfil creado correctamente")
--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import Profile
 # Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user("test", "test@gmail.com", "test1234")
+
+    def test_profile_exists(self):
+        exist = Profile.objects.filter(user__username = "test").exists()#Esto devuelve un booleano, si el usuario existe devuelve True, si no existe devulve false
+        self.assertEqual(exist, True)#Comprobar que exist == True, en cuyo caso pasará el test.
